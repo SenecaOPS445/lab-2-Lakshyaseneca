4e984580cf3b42e140ef5a05aab4961cce11cee6
Lakshya Kumar <lkumar12@myseneca.ca>
2024-05-17 12:54:02 -0400

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Lakshya Kumar
+# Author ID: lkumar12@myseneca.ca
+# Date Created: 2024/05/17
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
