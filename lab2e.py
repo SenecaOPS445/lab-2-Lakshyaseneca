f816049154ae88dc9f455b238d9e4c55a605a7bc
Lakshya Kumar <lkumar12@myseneca.ca>
2024-05-17 12:53:52 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Lakshya Kumar
+# Author ID: lkumar12@myseneca.ca
+# Date Created: 2024/05/17
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
